2c97d33460816b1201e56027faf32df700845a4e
dkrivaa <dannykrivaa@hotmail.com>
2024-09-18 09:22:23 +0300

    initial commit

--- mof_data.py
+++ mof_data.py
@@ -94,9 +94,9 @@ def get_annual_total(df, organization, budget):
 
 
 def drop_columns(df):
-    columns_to_drop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20,
-                       21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 37]
-    df = df.drop(columns=[columns_to_drop], axis=1)
+    columns_to_drop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20,
+                       21, 22, 23, 24, 25, 26, 27, 28, 29, 31, 32, 33, 34, 35, 36, 37]
+    df = df.drop(df.columns[columns_to_drop], axis=1)
     return df
 
 
