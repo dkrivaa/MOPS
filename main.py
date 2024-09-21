0e061addd51c8729f2ed0f33094dbe7e186b5a82
dkrivaa <dannykrivaa@hotmail.com>
2024-09-21 12:04:56 +0300

    initial commit

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ Run this file to update data from Ministry of Finance data on wix site (MOPS).
 At present, data is updated to 23.5.2024
 """
 
-from history import update_annual
+from history import update_annual, update_annual_special
 from takanot import update_takanot
 
-update_annual()
-update_takanot()
+# update_annual()
+update_annual_special()
+# update_takanot()
 
