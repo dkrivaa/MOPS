0bb86a8ebbd5db73c356f06bcab042524058b557
dkrivaa <dannykrivaa@hotmail.com>
2024-09-21 11:17:06 +0300

    initial commit

--- takanot.py
+++ takanot.py
@@ -2,7 +2,7 @@ from mof_data import get_data, get_takanot
 from wix_requests import takanot_request
 
 def update_takanot():
-    years = [2024]
+    years = [2022, 2023, 2024]
     for year in years:
         # All of ministry
         df = get_data(year, 'total', 'total')
